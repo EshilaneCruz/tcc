022520941dc32a364ce42108add2a2623494214b
cruz.eshilane <cruz.eshilane@cwi.com.br>
2018-10-17 23:12:47 -0300

    create trip partial

--- app/src/main/java/br/feevale/projetofinal/activities/StartTripActivity.kt
+++ app/src/main/java/br/feevale/projetofinal/activities/StartTripActivity.kt
@@ -2,12 +2,22 @@ package br.feevale.projetofinal.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.feevale.projetofinal.R
+import br.feevale.projetofinal.dialogs.DatePickerFragment
+import kotlinx.android.synthetic.main.activity_start_trip.*
 
 class StartTripActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_trip)
+
+        pickStartDateButton.setOnClickListener { showStartDatePickerDialog() }
+    }
+
+    fun showStartDatePickerDialog() {
+        val newFragment = DatePickerFragment()
+        newFragment.show(supportFragmentManager, "datePicker")
     }
 }
